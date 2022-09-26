2eff81b430b25b15cbf1bbfa2b24405afe8368ed
nick <weist.wei@gmail.com>
2022-09-26 12:17:44 +0800

    git init

--- src/SimpleHttpMethod.php
+++ src/SimpleHttpMethod.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SimpleHttp;
+namespace NickLabs\SimpleHttp;
 
 class SimpleHttpMethod {
 
--- src/SimpleHttpResponse.php
+++ src/SimpleHttpResponse.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SimpleHttp;
+namespace NickLabs\SimpleHttp;
 
 use Exception;
 use stdClass;
